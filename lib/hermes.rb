05c91103c24402995976124a4521afa3b44c7b96
Liss McCabe <liss@eristiccode.com>
2015-05-15 20:40:34 -0500

    Tie it all together

--- lib/hermes.rb
+++ lib/hermes.rb
@@ -2,8 +2,40 @@ require 'require_all'
 
 module Hermes
   require_all 'lib'
+  extend self
 
-  # TODO: (liss) implement
-  def self.run(filename)
+  def run_and_show(filename)
+    puts run(filename)
+  end
+
+  def run(filename)
+    begin
+      present(register(transactions(input_lines(file_lines(filename)))))
+    rescue FormatError
+      "Error: Your input doesn't appear to be formatted correctly. \n\n"\
+        "You entered:\n\n #{File.read(filename)}"
+    end
+  end
+
+  private
+
+  def file_lines(filename)
+    @file_lines ||= File.readlines(filename)
+  end
+
+  def input_lines(file_lines)
+    file_lines.map { |line| InputLine.new(line) }
+  end
+
+  def transactions(input_lines)
+    input_lines.map  { |il| Transaction.generate_from(il) }
+  end
+
+  def register(transactions)
+    Register.new(transactions: transactions)
+  end
+
+  def present(register)
+    register.print
   end
 end
--- spec/hermes_spec.rb
+++ spec/hermes_spec.rb
@@ -6,30 +6,46 @@ describe Hermes do
   end
 
   describe '.run' do
-    subject      { Hermes.run(input) }
-    let(:filename) { "example.txt" }
+    before do
+      allow(File).to receive(:readlines)
+        .with('filename.txt').and_return(input)
+    end
 
     let(:input) do
-      "Add Soos 4111111111111111 $1000\n"\
-        "Add Mabel 5454545454545454 $3000\n"\
-        "Add Dipper 1234567890123456 $2000\n"\
-        "Charge Soos $500\n"\
-        "Charge Soos $800\n"\
-        "Charge Mabel $7\n"\
-        "Credit Mabel $100\n"\
-        "Credit Dipper $200\n"
+      ["Add Soos 4111111111111111 $1000\n",
+        "Add Mabel 5454545454545454 $3000\n",
+        "Add Dipper 1234567890123456 $2000\n",
+        "Charge Soos $500\n",
+        "Charge Soos $800\n",
+        "Charge Mabel $7\n",
+        "Credit Mabel $100\n",
+        "Credit Dipper $200\n"]
     end
 
-    let(:output) do
-      "Mabel: $-93\n"\
+    subject { Hermes.run('filename.txt') }
+
+    context 'when the file is valid' do
+      let(:output) do
         "Dipper: error\n"\
-        "Soos: $500\n"
+          "Mabel: $3093\n"\
+          "Soos: -$300\n"
+      end
+
+      it 'produces the expected output' do
+        expect(subject).to eq(output)
+      end
     end
 
-    before { allow(File).to receive(:read).with(filename).and_return(input) }
+    context 'when the file is improperly formatted' do
+      let(:output) do
+        "Dipper: error\n"\
+          "Mabel: $3093\n"\
+          "Soos: -$300\n"
+      end
 
-    pending 'produces the expected output' do
-      expect(subject).to eq(output)
+      it 'produces the expected output' do
+        expect(subject).to eq(output)
+      end
     end
   end
 end
